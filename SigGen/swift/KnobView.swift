b5285708df731b511cccc9d8aae6aa541bcce431
Bill Farmer <williamjfarmer@yahoo.co.uk>
2018-03-30 17:35:41 +0100

    Add copyright

--- SigGen/swift/KnobView.swift
+++ SigGen/swift/KnobView.swift
@@ -5,6 +5,19 @@
 //  Created by Bill Farmer on 29/03/2018.
 //  Copyright © 2018 Bill Farmer. All rights reserved.
 //
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 
 import Cocoa
 
@@ -16,6 +29,7 @@ class KnobView: NSView
         super.draw(dirtyRect)
 
         // Drawing code here.
+        let indexSize = width / 32
         let gradient = NSGradient(colors: [NSColor.white, NSColor.darkGray])!
         let shade = NSBezierPath(ovalIn: dirtyRect)
         gradient.draw(in: shade, angle: 315) 
@@ -28,7 +42,7 @@ class KnobView: NSView
                                      byY: NSMidY(dirtyRect))
         (centre as NSAffineTransform).concat()
 
-        let indent = NSMakeRect(-4, -4, 8, 8)
+        let indent = NSMakeRect(-size / 2, -size / 2, size, size)
         let indentPath = NSBezierPath(ovalIn: indent)
 
         let y = NSMidY(inset) - NSMidY(inset) / 4
