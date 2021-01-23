cb90b96576e19053f8fcf2136b833aeb9fa49249
Bill Farmer <williamjfarmer@yahoo.co.uk>
2021-01-23 14:16:58 +0000

    Update travis

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 set -ev
-if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-    cd LMS/mac
-    make
-    cd ../../Scope/mac
-    make
-    cd ../../SigGen/mac
-    make
-    cd ../../SLMS/mac
-    make
-fi
 
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     sudo apt update
